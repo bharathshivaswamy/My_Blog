35e89ece4ba8a18925b9613ebbb02b48f3ffe786
bharathshivaswamy <bharathshivaswamy@gmail.com>
2018-07-02 09:26:02 -0700

    css error fixed

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,"deploy_to_server")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
